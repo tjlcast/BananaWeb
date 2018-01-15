0a2cd5dda6aa537a496dc3cfdfcc9cfaac5c722d
tangjialiang <phx108@sina.com>
2018-01-15 15:44:36 +0800

    web app start !!!

---
+++ test/aboutCoroutine.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import threading
+import asyncio
+
+
+@asyncio.coroutine
+def hello():
+    print('Hello world! (%s)' % threading.current_thread())
+    yield from asyncio.sleep(1)
+    print('Hello world! (%s)' % threading.current_thread())
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    tasks = [hello(), hello()]
+    loop.run_until_complete(asyncio.wait(tasks))
+    loop.close()
+
---
+++ www/orm/BananaOrm.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from www.orm.OrmObj import Field
+
+import asyncio
+import aiomysql
+
+logging.basicConfig(level=logging.INFO)
+
+
+# 创建连接池
+@asyncio.coroutine
+def create_pool(loop, **kw):
+    logging.info('create database connection pool..')
+    global __pool
+    __pool = yield from aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),
+        port=kw.get('host', 3306),
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['db'],
+        charset=kw.get("charset", "utf8"),
+        autocommit=kw.get('autocommit', True),
+        maxsize=kw.get('maxsize', 10),
+        minsize=kw.get('minsize', 1),
+        loop=loop
+    )
+
+
+# 数据库的查询
+@asyncio.coroutine
+def select(sql, args, size=None):
+    logging.info(sql, args)
+    global __pool
+    with (yield from __pool) as conn:
+        cur = yield from conn.cursor(aiomysql.DictCursor)
+        cur.execute(sql.repace('?', '%s'), args or ())
+        rs = []
+        if size:
+            rs = yield from cur.fetchmany(size)
+        else:
+            rs = yield from cur.fetchall()
+        yield from cur.close()
+        logging.info("rows returned. %s" % len(rs))
+        return rs
+
+
+# 数据库的动作
+@asyncio.coroutine
+def execute(sql, args):
+    logging.info(sql)
+    with (yield from __pool) as conn:
+        try:
+            cur = yield from conn.cursor()
+            yield from cur.execute(sql.replace('?', '%s'), args)
+            affected = cur.rowcount
+            yield from cur.close()
+        except BaseException as e:
+            raise
+        return affected
+
+
+# orm Model对象的构造类
+class ModelMetaclass(type):
+    def __new__(mcs, name, bases, attrs):
+        if name == 'Model':
+            return type.__new__(mcs, name, bases, attrs)
+
+        tableName = attrs.get('__table__', None) or name
+        logging.info('found model: %s (table: %s)' % (name, tableName))
+
+        mappings = dict()
+        fields = []
+        primaryKey = None
+        for k, v in attrs.items():
+            if isinstance(v, Field):
+                logging.info('  found mapping: %s ==> %s' % (k, v))
+                mappings[k] = v
+                if v.primary_key:
+
+                    if primaryKey:
+                        raise RuntimeError('Duplicate primary key for field: %s' % k)
+                    primaryKey = k
+                else:
+                    fields.append(k)
+        if not primaryKey:
+            raise RuntimeError('Primary key not found.')
+        for k in mappings.keys():
+            attrs.pop(k)
+        escaped_fields = list(map(lambda f: '`%s`' % f, fields))
+        attrs['__mappings__'] = mappings
+        attrs['__table__'] = tableName
+        attrs['__primary_key__'] = primaryKey
+        attrs['__fields__'] = fields
+
+        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (
+        tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (
+        tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
+        return type.__new__(mcs, name, bases, attrs)
+
+
+
